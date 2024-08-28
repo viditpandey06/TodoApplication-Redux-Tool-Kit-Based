1f4c14f3cc1fe07e01bdceb368fcbb62dd5af980
Vidit Pandey <viditpandey06@gmail.com>
2024-08-28 14:36:04 +0530

    Update Todo.jsx

--- ReduxToolKitToDoApp/src/components/Todo.jsx
+++ ReduxToolKitToDoApp/src/components/Todo.jsx
@@ -6,6 +6,8 @@ function Todos() {
   const todos = useSelector(state => state.todos)
   const dispatch = useDispatch()
 
+    
+
   return (
     <>
       <div>Todos</div>
@@ -35,16 +37,12 @@ function Todos() {
                 />
               </svg>
             </button>
-            <button
-              className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
-              onClick={() => dispatch(updateTodo(todo.id))}
-            > "✏️"
-            </button>
+           
           </li>
         ))}
       </ul>
     </>
-  )
+  );
 }
 
 export default Todos
